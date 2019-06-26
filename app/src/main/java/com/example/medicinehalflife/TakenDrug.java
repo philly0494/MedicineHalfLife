e46db50308d98181a9d1cc640d6feeae1bfe7ca8
Phong <phongngo91@gmail.com>
2019-06-26 16:32:29 -0400

    added radio group for dose selection.

--- app/src/main/java/com/example/medicinehalflife/TakenDrug.java
+++ app/src/main/java/com/example/medicinehalflife/TakenDrug.java
@@ -7,33 +7,33 @@ public class TakenDrug {
     int halflife;
     double maxlevel;
 
-    public TakenDrug(int hl){
+    public TakenDrug(int hl) {
         this.halflife = hl;
     }
-    public TakenDrug(Drug d)
-    {
+
+    public TakenDrug(Drug d) {
         this.halflife = d.HalflifeMinutes;
         this.Name = d.Name;
     }
+
     //
-    public void SimulateSingleDose(int initialDose){
-        concentrations = new double[60*24];  //minute-by-minute for 24 hours
-        concentrations[0]= initialDose;
+    public void SimulateSingleDose(int initialDose) {
+        concentrations = new double[60 * 24];  //minute-by-minute for 24 hours
+        concentrations[0] = initialDose;
         maxlevel = initialDose;
-        for(int i=1;i<concentrations.length;i++)
-        {
-            double decayRate = concentrations[i-1]*Math.log(2)/halflife;
-            concentrations[i] = concentrations[i-1] - decayRate;
+        for (int i = 1; i < concentrations.length; i++) {
+            double decayRate = concentrations[i - 1] * Math.log(2) / halflife;
+            concentrations[i] = concentrations[i - 1] - decayRate;
         }
     }
-    public void SimulateRepeatedDose(int initialDose, int repeatedDose,int frequency){
-        concentrations = new double[60*24];  //minute-by-minute for 24 hours
-        concentrations[0]= initialDose;
-        for(int i=1;i<concentrations.length;i++)
-        {
-            double decayRate = concentrations[i-1]*Math.log(2)/halflife;
-            concentrations[i] = concentrations[i-1] - decayRate;
-            if (i%frequency == 0){
+
+    public void SimulateRepeatedDose(int initialDose, int repeatedDose, int frequency) {
+        concentrations = new double[60 * 24];  //minute-by-minute for 24 hours
+        concentrations[0] = initialDose;
+        for (int i = 1; i < concentrations.length; i++) {
+            double decayRate = concentrations[i - 1] * Math.log(2) / halflife;
+            concentrations[i] = concentrations[i - 1] - decayRate;
+            if (i % frequency == 0) {
                 concentrations[i] += repeatedDose;
             }
             if (concentrations[i] > maxlevel) {
